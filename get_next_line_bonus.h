ee94773369e4dcc698221edf8fc538af879334fd
Ayşe Zehra Aydoğdu <129674249+zehraxaa@users.noreply.github.com>
2024-12-15 00:06:15 +0300

    Update get_next_line_bonus.h

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -24,8 +24,8 @@ size_t	ft_strlen(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strjoin(char *s1, char *s2);
 char	*get_next_line(int fd);
-char	*ft_getafter(char *stash);
-char	*ft_getbefore(char *stash);
-char	*ft_joinfunc(char *stash, char *buff, int fd);
+char	*getafter(char *stash);
+char	*getbefore(char *stash);
+char	*joinfunc(char *stash, char *buff, int fd);
 
 #endif
